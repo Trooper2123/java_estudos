8de348e4c3f5fe6039e0cb2f5722cadadcf8f6dc
Tereza B Niederauer <52414737+Trooper2123@users.noreply.github.com>
2025-02-04 14:37:14 +0000

    change: file names

--- src/main/java/org/com/tere/obternerdiploma/GeneratecertificationApplication.java
+++ src/main/java/org/com/tere/obternerdiploma/GeneratecertificationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ObtenerDiplomaApplication {
+public class GeneratecertificationApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ObtenerDiplomaApplication.class, args);
+    SpringApplication.run(GeneratecertificationApplication.class, args);
   }
 
 }
